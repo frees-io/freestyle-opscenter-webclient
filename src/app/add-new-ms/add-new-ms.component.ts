eec568e3f66d1c9c4dd95e47f53392a44937c52b
Juan Valencia Calvellido <juanvalencia@calvellido.es>
2017-11-28 11:26:29 +0100

    Add success dialog to add new ms form
    * Add a dialog that gets open when a new microservice gets introduced in the sytem.
    * Add Angular Material dependencies to fulfill this purpose.
    * Customize the way a matInput form control check its erroneous state, to be able to reset a form properly.

--- src/app/add-new-ms/add-new-ms.component.ts
+++ src/app/add-new-ms/add-new-ms.component.ts
@@ -1,20 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+import { AddNewMsDialogComponent } from './add-new-ms-dialog.component';
+
+/* Set error state when invalid control is dirty, but not submitted.
+ * The reason for this is that currently, in a reactive form, there is no way of
+ * fully reset a form state. You can reset the model itself, which is what FormGroup
+ * is able to control, but the submitted state is out of the bounds of it, hence
+ * the control could be marked as erroneous when they are actually pristine.
+ *
+ * The default ErrorStateMatcher implementation considers an error cases fulfilling:
+ * !!(control && control.invalid && (control.touched || (form && form.submitted)));
+*/
+export class CustomErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null): boolean {
+    return !!(control && control.invalid && control.dirty);
+  }
+}
+
 
 @Component({
   selector: 'frees-add-new-ms',
   templateUrl: './add-new-ms.component.html',
   styleUrls: ['./add-new-ms.component.scss']
 })
-export class AddNewMsComponent implements OnInit {
+export class AddNewMsComponent {
 
-  private newMsForm: FormGroup;
+  public newMsForm: FormGroup;
+  public matcher = new CustomErrorStateMatcher();
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private dialog: MatDialog) {
     this.createForm();
   }
-
-  ngOnInit() { }
 
   private createForm() {
     this.newMsForm = this.fb.group({
@@ -24,8 +43,15 @@ export class AddNewMsComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log('Form submitted:');
-    console.log(this.newMsForm.value);
+    this.openDialog();
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(AddNewMsDialogComponent, {
+      data: this.newMsForm.value
+    });
+
+    dialogRef.afterClosed().subscribe(_ => this.newMsForm.reset());
   }
 
 }
