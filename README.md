78ceaae6090de44ccdd8c5eb9b671accc51e22e3
Juan Valencia Calvellido <juanvalencia@calvellido.es>
2017-12-11 13:55:36 +0100

    Add node.js mock server
    * Add node.js code and directory
    * Update README

--- README.md
+++ README.md
@@ -20,7 +20,9 @@ export const environment = {
 
 ## Development server
 
-Run `npm start` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+Run `npm start` for a dev fronten app server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+Also for development purposes, a mock data server, built in node.js, is included. It can be installed by running `npm run mock-server:install` and started with `npm run mock-server`.
 
 ## Protocol buffers
 
---
+++ mock-server/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+
+app.use(function (req, res, next) {
+  console.log('middleware');
+  req.testing = 'testing';
+  return next();
+});
+
+app.get('/', function (req, res, next) {
+  console.log('get route', req.testing);
+  res.end();
+});
+
+app.ws('/', function (ws, req) {
+  ws.on('message', function (msg) {
+    console.log(msg);
+  });
+  ws.on('close', function () {
+    console.log('disconnection');
+  });
+  console.log('socket', req.testing);
+});
+
+setInterval(function () {
+  expressWs.getWss('/').clients.forEach(function (client) {
+    var blob = new Buffer('microservice1 ' + Date.now() + ' cassandra.topics ' + (Math.random() * 100));
+    client.send(blob);
+  });
+}, 2000);
+
+app.listen(3000);
